f2ae9ca1a63890375a64024b008537d988b4caf7
AbLlanos <abers2341@gmail.com>
2026-02-19 10:03:53 -0500

    version1.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChatAsistente } from './components/chat-asistente/chat-asistente';
+import { Home } from './components/home/home';
 
 export const routes: Routes = [
 
+    { path: '', component: Home, pathMatch: 'full' },
     { path: 'chatAsistente', component: ChatAsistente },
 
 ];
